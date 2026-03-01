0a5355417740c6ad11143639672ee026cc8acfa0
Saifuddinmonna <108563464+Saifuddinmonna@users.noreply.github.com>
2026-03-02 01:51:36 +0600

    medicine route  secton or phrase is completed for initially

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import helmet from 'helmet';
 import compression from 'compression';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
-//import medicineRoutes from './routes/medicine.routes.js';
+import medicineRoutes from './routes/medicine.routes.js';
 import orderRoutes from './routes/order.routes.js';
 
 const app = express();
@@ -24,7 +24,7 @@ app.use(morgan("dev"));
 // 6️⃣ Routes (👉 এখানেই routes বসবে)
 
 app.use('/api/auth', authRoutes);
-//app.use('/api/medicines', medicineRoutes);
+app.use('/api/medicines', medicineRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/users', userRoutes);
 
